a4eff847fdd831560f79b8d8532d15f3c0c9e62b
x-farkasle <x-farkasle@wizzair.com>
2022-05-01 19:38:42 +0200

    starter

---
+++ README.md
@@ -0,0 +1,42 @@
+# Nuxt 3 Minimal Starter
+
+Learning Nuxt 3 by creating this silly 'Baby Name Generator' web app.
+
+## Setup
+
+Make sure to install the dependencies:
+
+```bash
+# yarn
+yarn install
+
+# npm
+npm install
+
+# pnpm
+pnpm install --shamefully-hoist
+```
+
+## Development Server
+
+Start the development server on http://localhost:3000
+
+```bash
+npm run dev
+```
+
+## Production
+
+Build the application for production:
+
+```bash
+npm run build
+```
+
+Locally preview production build:
+
+```bash
+npm run preview
+```
+
+Checkout the [deployment documentation](https://v3.nuxtjs.org/docs/deployment) for more information.
---
+++ app.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="container">
+    <h1>Baby name generator</h1>
+    <p>Choose your options and click the "Find Names" button below</p>
+    <div class="options-container">
+      <div class="option">
+        <h4>1, Choose a gender</h4>
+        <div class="option-buttons">
+          <button
+            class="option option-left"
+            :class="options.gender === Gender.BOY && 'option-active'"
+            @click="options.gender = Gender.BOY"
+          >
+            Boy
+          </button>
+          <button
+            class="option"
+            :class="options.gender === Gender.UNISEX && 'option-active'"
+            @click="options.gender = Gender.UNISEX"
+          >
+            Unisex
+          </button>
+          <button
+            class="option option-right"
+            :class="options.gender === Gender.GIRL && 'option-active'"
+            @click="options.gender = Gender.GIRL"
+          >
+            Girl
+          </button>
+        </div>
+      </div>
+      <div class="option">
+        <h4>2, Choose the names popularity</h4>
+        <div class="option-buttons">
+          <button
+            class="option option-left"
+            :class="options.popularity === Popularity.UNIQUE && 'option-active'"
+            @click="options.popularity = Popularity.UNIQUE"
+          >
+            Unique
+          </button>
+          <button
+            class="option option-right"
+            :class="options.popularity === Popularity.TRENDY && 'option-active'"
+            @click="options.popularity = Popularity.TRENDY"
+          >
+            Trendy
+          </button>
+        </div>
+      </div>
+      <div class="option">
+        <h4>3, Choose names length</h4>
+        <div class="option-buttons">
+          <button
+            class="option option-left"
+            :class="options.length === Length.LONG && 'option-active'"
+            @click="options.length = Length.LONG"
+          >
+            Long
+          </button>
+          <button
+            class="option"
+            :class="options.length === Length.ALL && 'option-active'"
+            @click="options.length = Length.ALL"
+          >
+            All
+          </button>
+          <button
+            class="option option-right"
+            :class="options.length === Length.SHORT && 'option-active'"
+            @click="options.length = Length.SHORT"
+          >
+            Short
+          </button>
+        </div>
+      </div>
+    </div>
+    <button class="primary" @click="onSelectedNames">Find Names</button>
+
+    <div class="cards-container">
+      <div class="card" v-for="name in generatedNames" :key="name.id">
+        <h4>{{ name }}</h4>
+        <p @click="removeName(name)">x</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Gender, Popularity, Length, names } from '@/data';
+
+interface OptionsState {
+  gender: Gender;
+  popularity: Popularity;
+  length: Length;
+}
+
+const options = reactive<OptionsState>({
+  gender: 'Girl',
+  popularity: 'Unique',
+  length: 'Long',
+});
+
+const generatedNames = ref<string[]>([]);
+
+const onSelectedNames = () => {
+  const filteredNames = names
+    .filter(name => name.gender === options.gender)
+    .filter(name => name.popularity === options.popularity)
+    .filter(name =>
+      options.length === Length.ALL ? name : name.length === options.length
+    );
+
+  generatedNames.value = filteredNames.map(name => name.name);
+};
+
+const removeName = selectedName =>
+  (generatedNames.value = generatedNames.value.filter(
+    name => name !== selectedName
+  ));
+</script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  font-family: Arial, Helvetica, sans-serif;
+  color: rgb(27, 60, 138);
+  max-width: 50rem;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.container h1 {
+  font-size: 3rem;
+}
+
+.container .options-container {
+  background: rgb(255, 238, 236);
+  border-radius: 2rem;
+  padding: 1rem;
+  width: 95%;
+  margin: 0 auto;
+  margin-top: 4rem;
+  position: relative;
+}
+
+.option-container {
+  margin-bottom: 2rem;
+}
+
+.option-buttons .option {
+  background: white;
+  outline: 0.15rem solid rgb(249, 87, 89);
+  border: none;
+  padding: 0.75rem;
+  width: 12rem;
+  font-size: 1rem;
+  color: rgb(27, 60, 138);
+  cursor: pointer;
+  font-weight: 200;
+}
+
+.option-buttons .option-left {
+  border-radius: 1rem 0 0 1rem;
+}
+
+.option-buttons .option-right {
+  border-radius: 0 1rem 1rem 0;
+}
+
+.option-buttons .option-active {
+  background: rgb(249, 87, 89);
+  color: white;
+}
+
+.primary {
+  background-color: rgb(249, 87, 89);
+  color: white;
+  border-radius: 6.5rem;
+  border: none;
+  padding: 0.75rem 4rem;
+  font-size: 1rem;
+  margin-top: 1rem;
+  cursor: pointer;
+}
+
+.cards-container {
+  display: flex;
+  margin-top: 3rem;
+  gap: 1rem;
+  flex-wrap: wrap;
+}
+
+.card {
+  background-color: rgb(27, 60, 138);
+  width: 28%;
+  color: white;
+  border-radius: 1rem;
+  padding: 0.1rem;
+  margin-bottom: 1rem;
+  position: relative;
+}
+
+.card p {
+  position: absolute;
+  top: -20%;
+  left: 90%;
+  cursor: pointer;
+  color: rgba(255, 255, 255, 0.178);
+}
+</style>
